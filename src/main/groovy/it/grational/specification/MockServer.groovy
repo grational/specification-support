466a9911b3bfa4a7c9922b1b914bb8071385278f
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2021-10-25 00:12:13 +0200

    Adding the MockServer facility and updating the build configuration

---
+++ src/main/groovy/it/grational/specification/MockServer.groovy
@@ -0,0 +1,38 @@
+package it.grational.specification
+
+import groovy.lang.Delegate
+import groovy.transform.ToString
+import com.github.tomakehurst.wiremock.WireMockServer
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
+
+class MockServer {
+	String  protocol
+	String  host
+	Integer port
+	String  origin
+	String  path
+	String  body
+	URL     url
+
+	private final Map ok = [
+		code: 200,
+		body: '{"status":"OK"}'
+	]
+
+	@Delegate WireMockServer wms
+
+	MockServer(Map params) {
+		this.protocol = (params?.protocol ==~ 'https?') ? params.protocol : 'http'
+		this.host     = params?.host ?: 'localhost'
+		this.port     = params?.port ?: 1234
+		this.path     = params?.path ?: '/appropriate/path'
+		this.body     = params?.body ?: this.ok.body
+
+		this.origin   = "${protocol}://${host}:${port}"
+		this.url      = "${origin}${path}".toURL()
+		this.wms = (this.protocol == 'http')
+		         ? new WireMockServer(options().port(this.port))
+		         : new WireMockServer(options().httpDisabled(true).httpsPort(this.port))
+	}
+
+}
